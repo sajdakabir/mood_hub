16fce7e21336cb68a10296a2bc6c8108834ea623
sajdkabir <sajda.kbir@gmail.com>
2024-08-17 01:53:23 +0530

    feat: header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Fugaz_One, Inter } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
+const fugaz = Fugaz_One({ subsets: ["latin"], weight:['400'] });
 
 export const metadata: Metadata = {
   title: "mood",
@@ -14,13 +16,23 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const header = (
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+      <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className} >MoodHub</h1>
+    </header>
+  )
+
+  const footer = (
+    <footer>
+      foot
+    </footer>
+  )
   return (
     <html lang="en">
-
-      <body className={inter.className}>
-        <header>header</header>
+      <body className={ 'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col '+inter.className}>
+       {header}
         {children}
-        <footer>footer</footer>
+        {footer}
       </body>
 
     </html>
