35e72ffe30480bf0405c91b70e371ffdd43abbc7
sajdkabir <sajda.kbir@gmail.com>
2024-08-16 19:20:07 +0530

    feat: setup the layout and conponents

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,30 @@
 import Main from '@/components/Main'
 import React from 'react'
+import type { Metadata } from "next";
+import Login from '@/components/Login';
+import Dashboard from '@/components/Dashboard';
+
+export const metadata: Metadata = {
+  title: "mood–Dashboard",
+  description: "track your mood daily",
+};
+
 
 function DashboardPage() {
+  const isAuthenticated= false;
+  let children = (
+     <Login />
+  )
+
+  if (isAuthenticated) {
+    children =(
+      <Dashboard/>
+    )
+  }
+
     return(
       <Main className=''> 
-
+      {children}
       </Main>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import Main from "@/components/Main";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <Main className= ''>
-
+      <Hero />
     </Main>
   );
 }
