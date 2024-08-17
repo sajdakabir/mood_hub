d0d815097c4a0ebbbc7649a41cc216e9a5d74d0b
sajdkabir <sajda.kbir@gmail.com>
2024-08-18 01:05:16 +0530

    feat: moods btn

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -11,6 +11,14 @@ function Dashboard() {
     date: (new Date().toString())
   }
 
+  const moods = {
+    '$*@#^$': '😭',
+    'Sad': '🥲',
+    'Existing':'😃',
+    'Good':'😊',
+    'Elated':'😍'
+  }
+
   return (
     <div className='flex flex-col flex-1 gap-8 sm:gap-12 md:gap-16'>
       <div className='grid grid-cols-3 md:grid-cols-3 bg-indigo-50 p-4 gap-4 text-indigo-500 rounded-lg'>
@@ -24,6 +32,18 @@ function Dashboard() {
       })}
       </div>
       <h4 className={'text-5xl sm:text-6xl md:text-7xl text-center '+ fugaz.className}>How do you <span className='textGradient'>feel</span> today?</h4>
+
+      <div className='flex items-stretch flex-wrap gap-4'>
+        {Object.keys(moods).map((mood, moodIndex)=>{
+          return (
+            <button className={' p-4 px-5 rounded-2xl purpleShadow duration-200 bg-indigo-50 hover:bg-indigo-100 text-center items-center flex flex-col gap-2 flex-1'} key={moodIndex}>
+              <p className='text-4xl sm:text-5xl md:text-6xl'>{moods[mood]}</p>
+
+              <p className={'text-indigo-500 text-xs sm:text-sm md:text-base '+ fugaz.className}>{mood}</p>
+            </button>
+          )
+        })}
+      </div>
     </div>
   )
 }
